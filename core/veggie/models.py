032f8b0fc9c762226ad07ded5ba2936bdaf9ed22
Bigyan <luitelbigyan344@gmail.com>
2025-04-26 18:26:13 +0545

    recipie filter

--- core/veggie/models.py
+++ core/veggie/models.py
@@ -6,5 +6,6 @@ class recipie(models.Model):
     recepie_name=models.CharField(max_length=100)
     recepie_description=models.TextField()
     recepie_image=models.ImageField(upload_to="recepies/",null=True, blank=True)
+    recepie_view_count=models.IntegerField(default=1)
     
     
